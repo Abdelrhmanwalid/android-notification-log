ffd39c6d0b1b1891be28671c989c945893b4daa0
Abdelrhman Walid <abdelrhmanwalid1@gmail.com>
2019-01-14 03:52:34 +0200

    listen for deletion updates and refresh the notifications list

--- app/src/main/java/org/hcilab/projects/nlog/ui/BrowseActivity.java
+++ app/src/main/java/org/hcilab/projects/nlog/ui/BrowseActivity.java
@@ -1,7 +1,12 @@
 package org.hcilab.projects.nlog.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,10 +16,12 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import org.hcilab.projects.nlog.R;
+import org.hcilab.projects.nlog.service.NotificationHandler;
 
 public class BrowseActivity extends AppCompatActivity {
 
 	private RecyclerView recyclerView;
+	private BroadcastReceiver updateReceiver;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,15 +31,33 @@ public class BrowseActivity extends AppCompatActivity {
 		RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
 		recyclerView = findViewById(R.id.list);
 		recyclerView.setLayoutManager(layoutManager);
+
+		updateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                update();
+            }
+        };
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		update();
+
+		IntentFilter filter = new IntentFilter();
+        filter.addAction(NotificationHandler.DELETE_BROADCAST);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(updateReceiver, filter);
 	}
 
-	@Override
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(updateReceiver);
+    }
+
+    @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.browse, menu);
